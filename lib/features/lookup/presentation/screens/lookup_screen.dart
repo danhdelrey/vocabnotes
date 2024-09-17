1c3c7fdf902b62f43045a66a067ede82d97e6e13
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-17 08:06:06 +0700

    3

--- lib/features/lookup/presentation/screens/lookup_screen.dart
+++ lib/features/lookup/presentation/screens/lookup_screen.dart
@@ -34,15 +34,12 @@ class _LookupScreenState extends State<LookupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
+    return BlocProvider(
+      
+        
           create: (context) => WordInformationBloc(),
-        ),
-        BlocProvider(
-          create: (context) => SaveToLibraryBloc(),
-        ),
-      ],
+        
+        
       child: Builder(
         builder: (context) {
           return Scaffold(
@@ -77,71 +74,5 @@ class _LookupScreenState extends State<LookupScreen> {
     );
   }
 
-  Scaffold _buildSearchWordInformationError(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: _searchedWords.isNotEmpty
-            ? IconButton(
-                onPressed: () {
-                  String previousWord = _searchedWords.removeLast();
-                  context
-                      .read<WordInformationBloc>()
-                      .add(GetWordInformationEvent(word: previousWord));
-                },
-                icon: const Icon(HugeIcons.strokeRoundedArrowLeft01),
-              )
-            : Container(),
-      ),
-      body: const Center(
-        child: Text('something went wrong'),
-      ),
-    );
-  }
-
-  AppBar _buildTopBar(WordInformationLoaded state, BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      title: Text(state.englishWordModelList[0].name),
-      leading: _searchedWords.length > 1
-          ? IconButton(
-              onPressed: () {
-                _searchedWords.removeLast();
-                String previousWord = _searchedWords.removeLast();
-                context
-                    .read<WordInformationBloc>()
-                    .add(GetWordInformationEvent(word: previousWord));
-              },
-              icon: const Icon(HugeIcons.strokeRoundedArrowLeft01),
-            )
-          : Container(),
-      actions: [
-        IconButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (_) => AlertDialog(
-                title: const Text('Add to library?'),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Cancel')),
-                  FilledButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        context.read<SaveToLibraryBloc>().add(
-                            SaveWordToLibraryEvent(
-                                wordList: state.englishWordModelList));
-                      },
-                      child: const Text('Yes'))
-                ],
-              ),
-            );
-          },
-          icon: const Icon(HugeIcons.strokeRoundedNoteAdd),
-        ),
-      ],
-    );
-  }
+  
 }
--- lib/features/lookup/presentation/screens/lookup_word_information_screen.dart
+++ lib/features/lookup/presentation/screens/lookup_word_information_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vocabnotes/features/lookup/presentation/blocs/save_to_library/save_to_library_bloc.dart';
+import 'package:vocabnotes/features/lookup/presentation/blocs/word_information_bloc/word_information_bloc.dart';
 
 class LookupWordInformationScreen extends StatefulWidget {
   const LookupWordInformationScreen({super.key});
@@ -11,5 +13,73 @@ class _LookupWordInformationScreenState extends State<LookupWordInformationScree
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Scaffold _buildSearchWordInformationError(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: _searchedWords.isNotEmpty
+            ? IconButton(
+                onPressed: () {
+                  String previousWord = _searchedWords.removeLast();
+                  context
+                      .read<WordInformationBloc>()
+                      .add(GetWordInformationEvent(word: previousWord));
+                },
+                icon: const Icon(HugeIcons.strokeRoundedArrowLeft01),
+              )
+            : Container(),
+      ),
+      body: const Center(
+        child: Text('something went wrong'),
+      ),
+    );
+  }
+
+  AppBar _buildTopBar(WordInformationLoaded state, BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Text(state.englishWordModelList[0].name),
+      leading: _searchedWords.length > 1
+          ? IconButton(
+              onPressed: () {
+                _searchedWords.removeLast();
+                String previousWord = _searchedWords.removeLast();
+                context
+                    .read<WordInformationBloc>()
+                    .add(GetWordInformationEvent(word: previousWord));
+              },
+              icon: const Icon(HugeIcons.strokeRoundedArrowLeft01),
+            )
+          : Container(),
+      actions: [
+        IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                title: const Text('Add to library?'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel')),
+                  FilledButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        context.read<SaveToLibraryBloc>().add(
+                            SaveWordToLibraryEvent(
+                                wordList: state.englishWordModelList));
+                      },
+                      child: const Text('Yes'))
+                ],
+              ),
+            );
+          },
+          icon: const Icon(HugeIcons.strokeRoundedNoteAdd),
+        ),
+      ],
+    );
   }
 }
