5e818a987da780eca3f6494271f3002fb2d30f77
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-17 12:12:22 +0700

    1

--- lib/features/short_stories/presentation/screens/short_stories_screen.dart
+++ lib/features/short_stories/presentation/screens/short_stories_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class ShortStoriesScreen extends StatelessWidget {
   const ShortStoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Short stories'),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(HugeIcons.strokeRoundedEdit02))
+        ],
+      ),
+      body: const Placeholder(),
+    );
   }
 }
