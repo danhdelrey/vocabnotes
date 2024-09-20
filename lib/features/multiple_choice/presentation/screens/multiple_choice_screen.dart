fe7874a1632b4a1645e3bd9e84c4c55b1c1ff35f
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-20 18:33:56 +0700

    1

--- lib/features/multiple_choice/presentation/screens/multiple_choice_screen.dart
+++ lib/features/multiple_choice/presentation/screens/multiple_choice_screen.dart
@@ -5,6 +5,59 @@ class MultipleChoiceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Multiple choice'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15, top: 40),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('1/15'),
+                Text(
+                  'in the nick of time',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _buildChoice(context),
+                _buildChoice(context),
+                _buildChoice(context),
+                _buildChoice(context)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildChoice(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(5),
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).colorScheme.outlineVariant,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+                'definitiondefinitiondefinitiondefinitiondefinitiondefinitiondefinitiondefinitiondefinit'),
+          ),
+        ),
+      ),
+    );
   }
 }
