19a3a2e43f1df42aa5f903b0fd247f7994b7f9d0
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-24 15:58:04 +0700

    bloc builder

--- lib/features/lookup/presentation/widgets/definition_with_example.dart
+++ lib/features/lookup/presentation/widgets/definition_with_example.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:vocabnotes/features/lookup/presentation/blocs/translate_definitions/translate_definitions_bloc.dart';
 
 class DefinitionWithExample extends StatelessWidget {
@@ -26,21 +27,81 @@ class DefinitionWithExample extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '> $definition',
-                  style: const TextStyle().copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                if (example != null)
-                  Text(
-                    'E.g. $example',
-                  ),
-              ],
+            child: BlocBuilder<TranslateDefinitionsBloc, TranslateDefinitionsState>(
+              builder: (context, state) {
+                if(state is TranslateDefinitionsInitial){
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '> $definition',
+                        style: const TextStyle().copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (example != null)
+                        Text(
+                          'E.g. $example',
+                        ),
+                    ],
+                  );
+                }else if(state is TranslateDefinitionsInProgress){
+                  return Skeletonizer(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '> $definition',
+                          style: const TextStyle().copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        if (example != null)
+                          Text(
+                            'E.g. $example',
+                          ),
+                      ],
+                    ),
+                  );
+                }else if(state is TranslateDefinitionsSuccess){
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '> ${state.translatedDefinition}',
+                        style: const TextStyle().copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (example != null)
+                        Text(
+                          'E.g. ${state.translatedExample}',
+                        ),
+                    ],
+                  );
+                }else {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '> $definition',
+                        style: const TextStyle().copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (example != null)
+                        Text(
+                          'E.g. $example',
+                        ),
+                    ],
+                  );
+                }
+                
+              },
             ),
           ),
         );
