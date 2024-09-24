47a36680aaffcc0e997d5d90019dcb02f5910fc3
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-24 15:38:14 +0700

    border

--- lib/features/lookup/presentation/widgets/definition_with_example.dart
+++ lib/features/lookup/presentation/widgets/definition_with_example.dart
@@ -14,6 +14,7 @@ class DefinitionWithExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {},
+      borderRadius: BorderRadius.circular(8),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
