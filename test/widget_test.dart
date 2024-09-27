5f841a6977c687d02d887cc513e66723ac48d940
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-27 21:44:53 +0700

    refactor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vocabnotes/app/main.dart';
+import 'package:vocabnotes/main.dart';
 import 'package:vocabnotes/view/widgets/bottom_navigation.dart';
 
 void main() {
