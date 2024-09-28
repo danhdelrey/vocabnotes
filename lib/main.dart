2e058b1d42f3d0a2d0dc4390ac717009814e75a7
danhdelrey <duonghoaidanh21092003@gmail.com>
2024-09-28 21:28:57 +0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vocabnotes/app/routes.dart';
 import 'package:vocabnotes/app/theme.dart';
 import 'package:vocabnotes/bloc/cubit/theme_switch_cubit.dart';
+import 'package:vocabnotes/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     BlocProvider(
       create: (context) => ThemeSwitchCubit()..setInitialTheme(),
